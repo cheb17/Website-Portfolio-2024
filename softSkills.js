ab79241674df434315d7a7a3f7863fadcb1477d1
chelsia banez <59606947+cheb17@users.noreply.github.com>
2024-12-21 02:31:30 +0800

    Add files via upload

---
+++ softSkills.js
@@ -0,0 +1,9 @@
+function openSoftSkills() {
+    document.getElementById("mySoftSkills").style.display = "block";
+  }
+
+  function closeForm() {
+    document.getElementById("mySoftSkills").style.display = "none";
+  }
+
+  
---
+++ techSkills.js
@@ -0,0 +1,8 @@
+function openTechSkills() {
+    document.getElementById("myTechSkills").style.display = "block";
+  }
+
+  function closeTech() {
+    document.getElementById("myTechSkills").style.display = "none";
+  }
+  
